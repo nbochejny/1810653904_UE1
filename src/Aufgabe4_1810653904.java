df927f0d917a5a19c56fe1df45a35b86f359822c
nbochejny <nbochejny@gmail.com>
2018-11-22 05:46:33 +0100

    Ende.

---
+++ src/Aufgabe4_1810653904.java
@@ -0,0 +1,10 @@
+public class Aufgabe4_1810653904 {
+    public static void main(String[] args){
+        int zahl1 = 50;
+        int zahl2 = 100;
+        System.out.println(++zahl1 * zahl2++);
+        System.out.println(zahl1 + zahl2);
+    }
+}
+//Als Präfix (vor der Variable) -> verändert Variableninhalt auf Anhieb
+//Als Postfix (nach der Variable) -> verändert Variableninhalt nach komplettem Programmdurchlauf
