8f0f75d98acb081563b218ac1cafafab0851f0eb
nbochejny <nbochejny@gmail.com>
2018-11-22 04:09:13 +0100

    Übung2

---
+++ src/Aufgab2_1810653904.java
@@ -0,0 +1,15 @@
+public class Aufgab2_1810653904 {
+    public static void main(String[] args){
+        String einBuchstabe = ("Buchstabenwert"); //Zeichen
+        int eineZahl = (666); //16bit Zahl
+        byte andereZahl = (2); //8Bit große Zahl
+        boolean ergebnis = eineZahl>andereZahl; //Wahr oder Falsch
+        double gleitkommazahl = (3.14); //Mehr Gleitkommastellen
+        long großeZahl = (1200000); //sehr große Zahl
+        char zeichen = 'A'; //Unicode um Buchstaben festzulegen
+        short kleineZahl = (88); //kleinere Zahl
+        float bessereZahl = 123.35f; //weniger Nachkommastellen
+        final int KONSTANTE = 218; //Variablen sind veränderbar, eine Konstante bleibt und wird nicht verändert (Beim ableiten fallen zahlen weg = Konstante)
+
+    }
+}
