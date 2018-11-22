afee9c2924ecd1c945d994ac2aa4a16f9b87e8c7
nbochejny <nbochejny@gmail.com>
2018-11-22 04:53:25 +0100

    Übung3Operatoren

---
+++ src/Aufgabe3_1810653904.java
@@ -0,0 +1,26 @@
+public class Aufgabe3_1810653904 {
+    public static void main(String[] args){
+        int zahl1 = 10;
+        int zahl2 = 20;
+            --zahl2;
+        int zahl3 = 30;
+            ++zahl3;
+        double zahl4 = 4.32;
+        double zahl5 = 5.43;
+        double zahl6 = 6.54;
+            int ergebnis;
+                ergebnis=zahl1+zahl2;
+                int ergebnis2;
+                ergebnis2=zahl1 / zahl3;
+            double ergebnis3;
+                ergebnis3=zahl4*zahl2;
+            int ergebnis4;
+                ergebnis4=zahl1%zahl2;
+        System.out.println(ergebnis);
+        System.out.println(ergebnis2);
+        System.out.println(ergebnis3);
+        System.out.println(ergebnis4);
+        System.out.println(zahl3);
+        System.out.println(zahl2);
+    }
+}
