8d97daf11713e3a5c704bf1e995720ab20bb7817
nbochejny <nbochejny@gmail.com>
2018-11-22 05:22:04 +0100

    Übung4

---
+++ src/Aufgabe5_1810653904.java
@@ -0,0 +1,56 @@
+public class Aufgabe5_1810653904 {
+    public static void main(String[] args){
+        int zahl1 = 10;
+        int zahl2 =20;
+        if (zahl1 < zahl2) {
+            System.out.println("Zahl1 is smaller than Zahl2");
+        }
+        else{
+            System.out.println("Zahl1 is bigger than Zahl2");
+        }
+        if (zahl2 < zahl1){
+            System.out.println("Zahl2 is smaller than Zahl1");
+        }
+        else {
+            System.out.println("Zahl2 is bigger than Zahl1");
+        }
+        if (zahl1 > zahl2) {
+            System.out.println("Zahl1 is bigger than Zahl2");
+        }
+        else{
+            System.out.println("Zahl1 is smaller than Zahl2");
+        }
+        if (zahl2 > zahl1){
+            System.out.println("Zahl2 is bigger than Zahl1");
+        }
+        else{
+            System.out.println("Zahl2 is smaller than Zahl1");
+        }
+        if (zahl1 <= zahl2){
+            System.out.println("Zahl1 is smaller or equal to Zahl2");
+        }
+        else{
+            System.out.println("Zahl1 is bigger than Zahl2");
+        }
+        if (zahl2 <= zahl1){
+            System.out.println("Zahl2 is smaller or equal to Zahl1");
+        }
+        else {
+            System.out.println("Zahl2 is bigger than Zahl1");
+        }
+        if (zahl1 >= zahl2){
+            System.out.println("Zahl1 is bigger or equal to Zahl2");
+        }
+        else {
+            System.out.println("Zahl1 is smaller than Zahl2");
+        }
+        if (zahl2 >= zahl1){
+            System.out.println("Zahl2 is bigger or equal to Zahl1");
+        }
+        else{
+            System.out.println("Zahl2 is smaller than Zahl1");
+        }
+        System.out.println(zahl1==zahl2);
+        System.out.println(zahl1!=zahl2);
+    }
+}
